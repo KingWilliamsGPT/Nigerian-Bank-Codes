7993bced78b31f8e2da6c764c7693326c1f20871
KingWilliamsGPT <williamusanga22@gmail.com>
2025-08-21 19:10:24 +0100

    Added: Paystack supported banks

--- _t.py
+++ _t.py
@@ -1,4 +1,5 @@
 import string
+import json
 
 
 def v(text):
@@ -14,3 +15,20 @@ def v(text):
                 continue
             pp += i
     return pp.strip().upper().replace(' ', '_')
+
+
+def save_dict(d, f_name):
+    with open(f_name, 'w') as f:
+        f.write(json.dumps(d))
+
+
+def paystack_save_dict(d: dict, f_name: str):
+     data = d['data']
+     k = {}
+     for bank in data:
+         k[bank['id']] = {
+             'bank_name': bank['name'],
+             'label': v(bank['name']),
+             'paystack_exact_data': bank
+         }
+     save_dict(k, f_name)
